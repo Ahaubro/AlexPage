809c8556c1a6f4473c133be7e07f14a0b704795c
Ahaubro <alex_haubro@hotmail.com>
2022-08-02 17:15:10 +0200

    Added more styling

--- AlexPage/Controllers/HomeController.cs
+++ AlexPage/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace AlexPage.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
