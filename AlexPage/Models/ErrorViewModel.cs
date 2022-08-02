2a01049e47173fbd9c6d59a442cfad79e376d281
Ahaubro <alex_haubro@hotmail.com>
2022-08-02 14:53:41 +0200

    Add project files.

---
+++ AlexPage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AlexPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
